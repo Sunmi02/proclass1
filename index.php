0c9887819295b681bfc12ec5b8e1b145af43c312
Sunmi02 <artgifg@gmail.com>
2026-06-09 21:22:58 -0600

    commit UNO

--- index.php
+++ index.php
@@ -3,3 +3,8 @@ require_once ('vistas/vista_superior.php');
 ?>
 
 <h1> Inicio </h1>
+
+
+<?php
+require_once ('vistas/vista_inferior.php');
+?>  
--- vistas/vista_superior.php
+++ vistas/vista_superior.php
@@ -1,1 +1,26 @@
-<h1> Inicio </h1>
+<!DOCTYPE html>
+<html lang="en">    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Proyecto</title>
+    
+</head>
+<body>
+    <header>
+       <menu>
+           <div class="menu-izquierda">
+               <a href="./index.php">
+                     <img src="img/cine.png" alt="Logo">  
+               </a>
+              
+           </div>
+              <div class="menu-derecha">
+                <a href="./index.php">Inicio</a>
+                <a href="./peliculas.php">Peliculas</a>
+                <a href="./series.php">Series</a>
+                <a href="./novedades.php">Novedades</a>
+              </div>
+           </menu>
+        </header>
